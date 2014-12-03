1447fa046a4d023cdb1c14211960e6767ca256aa
Hunter Chapman <bootcoder@gmail.com>
2014-12-02 18:42:01 -0800

    add test for each item to ensure it has a -- NAME - QUALITY - SELL_IN -- values

--- spec/gilded_rose_spec.js
+++ spec/gilded_rose_spec.js
@@ -1,7 +1,19 @@
 describe("Gilded Rose", function() {
 
-  it("should do something", function() {
-    update_quality();
-  });
+	describe("item", function(){
+		it("should have a sell_in value", function() {
+			var item = new Item("testItem", 5, 10);
+			expect(item.sell_in).toBe(5);
+		})
+		it("should have a name", function() {
+			var item = new Item("testItem", 5, 10);
+			expect(item.name).toBe("testItem");
+		})
+		it("should have a quality", function() {
+			var item = new Item("testItem", 5, 10);
+			expect(item.quality).toBe(10);
+		})
+	});
 
 });
+
