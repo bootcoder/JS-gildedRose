0c52bb911d86c58dc19eb42787ab300ef591c59b
Hunter Chapman <bootcoder@gmail.com>
2014-12-02 19:41:57 -0800

    update before each with new standard and non standard items -- add tests for brie and sulfruas

--- spec/gilded_rose_spec.js
+++ spec/gilded_rose_spec.js
@@ -24,12 +24,16 @@ describe("Gilded Rose", function() {
 
 	describe("updateQuality", function() {
 		
+		beforeEach(function(){
+			items = [];
+			items.push(new Item('Test Dexterity Vest', 10, 20));
+			items.push(new Item('Aged Brie', 2, 0));
+			items.push(new Item('Sulfuras, Hand of Ragnaros', 0, 80));
+
+		});
+
 		describe("updating the quality for basic items", function(){
 
-			beforeEach(function(){
-				items = [];
-				items.push(new Item('Test Dexterity Vest', 10, 20));
-			});
 
 			it("should lower sell_in value upon execution", function() {
 				var testItemSellIn = items[0].sell_in;
@@ -50,9 +54,49 @@ describe("Gilded Rose", function() {
 				expect(items[0].quality).toBe(testQuality-2);
 			});
 
+			it("should never have a quality with a negative value", function(){
+				items[0].quality = 0;
+				updateQuality();
+				expect(items[0].quality).toBe(0);
+			});
+		  
 		});
 
-		// describe("updating the quality of ")
+		describe("Updating Quality for Brie", function(){
+
+		  // - "Aged Brie" actually increases in *quality* the older it gets
+			it("increases in quality with each invocation of updateQuality", function(){
+				var brieQuality = items[1].quality;
+				updateQuality();
+				expect(items[1].quality).toBe(brieQuality+1);
+			});
+
+		  // - The *quality* of an item is never more than 50
+		  it("should never have a quality more than 50", function(){
+		  	items[1].quality = 50;
+		  	updateQuality();
+		  	expect(items[1].quality).toBe(50);
+		  });
+
+		});
+
+		describe("Updating the quality for Sulfuras", function(){
+
+			// - "Sulfuras", being a legendary item, never has to be sold nor does it decrease in *quality*
+			it("never decreases in quality", function(){
+				var sulfurasQuality = items[2].quality;
+				updateQuality();
+				expect(items[2].quality).toBe(sulfurasQuality);
+			});
+
+			it("is not affected by sell_in value", function(){
+				var sulfurasSellIn = items[2].sell_in;
+				updateQuality();
+				expect(items[2].sell_in).toBe(sulfurasSellIn);
+			});
+
+		});
+
 
 	});
 
